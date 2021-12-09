2b1842f49bb7f89e94d282fb53a5aebc3923eaf0
Bart Hogenes <barthogenes@gmail.com>
2021-12-09 13:37:13 +0100

    Finish Day 2 Part 1

--- adventofcode2021/dive.py
+++ adventofcode2021/dive.py
@@ -1,5 +1,18 @@
 def dive_part_1(input: str):
-    return 0
+    horizontal_position = 0
+    depth = 0
+    for line in input.splitlines():
+        words = line.split(" ")
+        if words[0] == "forward":
+            horizontal_position += int(words[1])
+
+        if words[0] == "down":
+            depth += int(words[1])
+
+        if words[0] == "up":
+            depth -= int(words[1])
+
+    return horizontal_position * depth
 
 
 def dive_part_2(input: str):
